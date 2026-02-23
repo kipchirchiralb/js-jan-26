08f67a18505bc2263dde9da0ef1db345c953557b
Albert <albertkipchirchir02@gmail.com>
2026-02-23 15:43:32 +0300

    varaibles,datatypes,operators

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+// In js data can be of different foramats/types 
+// number, string, boolean
+let year = 2026
+let officialEmail = 'albert@eldohub.co.ke4567890#&^%^&'
+let isLoggedIn = true
+
+// object, array, date
+
+// an object a collection of key value pairs of relted data e.g. eldohub info
+const eldohub = {
+    officalName: "Eldohub Innovations and Technologies Ltd",
+    location: "Eastern Avenue, Eldoret",
+    yearOfInception: 2017,
+    isSchool: true,
+    isUniveristy: false
+}
+
+// an array is a ordered(indexed) list of data
+const students = ["hezbon","nicole","jeremy"]
---
+++ variables.js
@@ -0,0 +1,13 @@
+// A variable is a container used to store data in memory
+// in javascript , varaibles are created using let, const or var
+// var is the old way of creating variables
+// let allows creating varaibles whose values(the data) can change
+// const is for storing data that does not change in the lifetime of an application on memory/ram
+
+let score = 9000;
+const interestRate = 15;
+
+var username = "kevin";
+
+score = 800;
+// interestRate = 10;
